ce3f927c0717f20406fdde9b1857eea518e028f7
Son-Gyeongi <thsruddl77@gmail.com>
2023-10-24 14:21:31 +0900

    [명언 앱 만들기] - 5단계 / 목록

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,5 +1,7 @@
 package com.ll;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class Main {
@@ -10,6 +12,7 @@ public class Main {
         System.out.println("==명언 앱==");
         String str; // 입력값
         int sequence = 0;
+        ArrayList arrayList = new ArrayList<>();
 
         do {
             System.out.print("명령 ) ");
@@ -21,11 +24,20 @@ public class Main {
                 System.out.print("작가 : ");
                 String author = s.nextLine();
                 System.out.println(++sequence + "번 명언이 등록되었습니다.");
+                arrayList.add(sequence + " / " + author + " / " + wiseSaying);
             }
             if (str.equals("종료")) {
                 System.exit(0);
             }
 
+            if (str.equals("목록")) {
+                System.out.println("번호 / 작가 / 명언");
+                System.out.println("----------------------");
+                Collections.reverse(arrayList);
+                for (int i = 0; i < arrayList.size(); i++) {
+                    System.out.println(arrayList.get(i));
+                }
+            }
         } while (true);
     }
 }
