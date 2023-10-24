c1ed2cad13036a60f018acdb5c37bc1183be73c3
Son-Gyeongi <thsruddl77@gmail.com>
2023-10-24 12:57:43 +0900

    [명언 앱 만들기] - 1단계 / 종료

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -4,5 +4,11 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        // Scanner 선언
+        Scanner s = new Scanner(System.in); // 키보드에서 입력을 받을 수 있는 Scanner 객체를 하나 생성
+
+        System.out.println("==명언 앱==");
+        System.out.print("명령 ) ");
+        String str = s.nextLine(); // 입력 받은 데이터 한 줄을 읽어서, String으로 리턴
     }
 }
