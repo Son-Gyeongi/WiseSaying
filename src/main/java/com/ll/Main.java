5bfd77d5247ebedd3caaf9b9232d01166b631f8d
Son-Gyeongi <thsruddl77@gmail.com>
2023-10-24 13:09:27 +0900

    [명언 앱 만들기] - 3 / 등록시 생성된 명언 번호 노출

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -8,7 +8,7 @@ public class Main {
         Scanner s = new Scanner(System.in); // 키보드에서 입력을 받을 수 있는 Scanner 객체를 하나 생성
 
         System.out.println("==명언 앱==");
-        String str;
+        String str; // 입력값
 
         do {
             System.out.print("명령 ) ");
@@ -19,6 +19,7 @@ public class Main {
                 String wiseSaying = s.nextLine();
                 System.out.print("작가 : ");
                 String author = s.nextLine();
+                System.out.println("1번 명언이 등록되었습니다.");
             }
             if (str.equals("종료")) {
                 System.exit(0);
