5c566175c8bd098320863e719c9308fcf3370fdb
Son-Gyeongi <thsruddl77@gmail.com>
2023-10-24 13:05:55 +0900

    [명언 앱 만들기] - 2단계 / 등록

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -8,7 +8,22 @@ public class Main {
         Scanner s = new Scanner(System.in); // 키보드에서 입력을 받을 수 있는 Scanner 객체를 하나 생성
 
         System.out.println("==명언 앱==");
-        System.out.print("명령 ) ");
-        String str = s.nextLine(); // 입력 받은 데이터 한 줄을 읽어서, String으로 리턴
+        String str;
+
+        do {
+            System.out.print("명령 ) ");
+            str = s.nextLine(); // 입력 받은 데이터 한 줄을 읽어서, String으로 리턴
+
+            if (str.equals("등록")) {
+                System.out.print("명언 : ");
+                String wiseSaying = s.nextLine();
+                System.out.print("작가 : ");
+                String author = s.nextLine();
+            }
+            if (str.equals("종료")) {
+                System.exit(0);
+            }
+
+        } while (true);
     }
 }
