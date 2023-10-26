08674d17c011d5373f014ef81750fa14e2a80608
Son-Gyeongi <thsruddl77@gmail.com>
2023-10-26 09:43:27 +0900

    혼자서 5단계 목록 ë§생성

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -1,11 +1,14 @@
 package com.ll;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 // 프로그램 중심
 public class App {
     void run() {
         int sequence = 0;
+        List<String> list = new ArrayList<>(); // 명언 목록
         System.out.println("==명언==");
 
         while (true) {
@@ -18,10 +21,19 @@ public class App {
                 break;
             } else if (cmd.equals("등록")) {
                 System.out.print("명언 : ");
-                scanner.nextLine();
+                String saying = scanner.nextLine();
                 System.out.print("작가 : ");
-                scanner.nextLine();
+                String author = scanner.nextLine();
                 System.out.println(++sequence + "번 명언이 등록되었습니다.");
+                String str = sequence + " / " + author + " / " + saying;
+                list.add(str);
+            } else if (cmd.equals("목록")) {
+                System.out.println("번호 / 작가 / 명언");
+                System.out.println("----------------------");
+                // 목록 내림차순으로 보여주기
+                for (int i = 0; i < list.size(); i++) {
+                    System.out.println(list.get((list.size()-1) - i));
+                }
             }
         }
     }
