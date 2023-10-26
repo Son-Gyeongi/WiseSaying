6511695b47bef27325ebf62bc62eadcb2570ce9f
Son-Gyeongi <thsruddl77@gmail.com>
2023-10-26 13:31:23 +0900

    강사님 따라서 5단계 목록 완성

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -30,9 +30,18 @@ public class App {
                 lastQuotationId++;
                 int id = lastQuotationId;
 
-                System.out.println(id + "번 명언이 등록되었습니다.");
+                System.out.printf("%d번 명언이 등록되었습니다.\n", id);
                 Quotation quotation = new Quotation(id, content, authorName);
                 quotations.add(quotation);
+            } else if (cmd.equals("목록")) {
+                System.out.println("번호 / 작가 / 명언");
+                System.out.println("----------------------");
+
+                // 목록 내림차순으로 보여주기
+                for (int i = quotations.size() - 1; i >= 0; i--) {
+                    Quotation quotation = quotations.get(i);
+                    System.out.printf("%d / %s / %s\n", quotation.id, quotation.authorName, quotation.content);
+                }
             }
         }
     }
