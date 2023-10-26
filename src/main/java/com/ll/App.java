ab1a4c998cc0b2f6de991b885755f16591aa1eb0
Son-Gyeongi <thsruddl77@gmail.com>
2023-10-26 13:12:06 +0900

    강사님 따라서 등록까지

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -7,9 +7,10 @@ import java.util.Scanner;
 // 프로그램 중심
 public class App {
     void run() {
-        int sequence = 0;
-        List<String> list = new ArrayList<>(); // 명언 목록
+        int lastQuotationId = 0;
         System.out.println("==명언==");
+
+        List<Quotation> quotations = new ArrayList<>();
 
         while (true) {
             System.out.print("명령) ");
@@ -21,29 +22,17 @@ public class App {
                 break;
             } else if (cmd.equals("등록")) {
                 System.out.print("명언 : ");
-                String saying = scanner.nextLine();
-                System.out.print("작가 : ");
-                String author = scanner.nextLine();
-                System.out.println(++sequence + "번 명언이 등록되었습니다.");
-                String str = sequence + " / " + author + " / " + saying;
-                list.add(str);
-            } else if (cmd.equals("목록")) {
-                System.out.println("번호 / 작가 / 명언");
-                System.out.println("----------------------");
-                // 목록 내림차순으로 보여주기
-                for (int i = 0; i < list.size(); i++) {
-                    System.out.println(list.get((list.size()-1) - i));
-                }
-            } else if (cmd.equals("삭제?id=")) {
-                String deleteNum = scanner.nextLine();
-                for (int i = 0; i < list.size(); i++) {
-                    String listNum = list.get(i).substring(0,1);
+                String content = scanner.nextLine();
 
-                    if (deleteNum.equals(listNum)) {
-                        list.remove(i);
-                        System.out.println(listNum + "번 명언이 삭제되었습니다.");
-                    }
-                }
+                System.out.print("작가 : ");
+                String authorName = scanner.nextLine();
+
+                lastQuotationId++;
+                int id = lastQuotationId;
+
+                System.out.println(id + "번 명언이 등록되었습니다.");
+                Quotation quotation = new Quotation(id, content, authorName);
+                quotations.add(quotation);
             }
         }
     }
