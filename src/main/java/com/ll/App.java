0401c2015acfde1cc37bcd2d6d3122c2923648be
Son-Gyeongi <thsruddl77@gmail.com>
2023-10-29 22:21:49 +0900

    quotation practice1 registration

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -5,14 +5,20 @@ import java.util.Scanner;
 public class App {
 
     void run() {
-        System.out.println("==명언 앱==");
-        System.out.println("명령) ");
-
         Scanner scanner = new Scanner(System.in);
-        String cmd = scanner.nextLine();
+        System.out.println("==명언 앱==");
 
-        if (cmd.equals(" 종료")) {
-            return;
+        while (true) {
+            System.out.print("명령) ");
+            String cmd = scanner.nextLine();
+            if (cmd.equals("종료")) {
+                break;
+            } else if (cmd.equals("등록")) {
+                System.out.print("명언 : ");
+                scanner.nextLine();
+                System.out.print("작가 : ");
+                scanner.nextLine();
+            }
         }
     }
 }
