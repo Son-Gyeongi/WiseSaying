57211b0a71f76ab32a608a6bc4d063484286452b
Son-Gyeongi <thsruddl77@gmail.com>
2023-10-29 21:13:34 +0900

    quotation practice1 ãstop

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -1,8 +1,18 @@
 package com.ll;
 
+import java.util.Scanner;
+
 public class App {
 
     void run() {
+        System.out.println("==명언 앱==");
+        System.out.println("명령) ");
 
+        Scanner scanner = new Scanner(System.in);
+        String cmd = scanner.nextLine();
+
+        if (cmd.equals(" 종료")) {
+            return;
+        }
     }
 }
