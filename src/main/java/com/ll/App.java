7f180adf33636dfbcb3c0b2f9de9efc2651c86d8
Son-Gyeongi <thsruddl77@gmail.com>
2023-11-04 16:32:17 +0900

    quotation practice2, step 4  / quotationId increase

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class App {
     public void run() {
         System.out.println("== 명언 앱 ==");
+        int quotationId = 0;
 
         while (true) {
             System.out.print("명령) ");
@@ -20,7 +21,10 @@ public class App {
                 System.out.print("작가 : ");
                 s.nextLine();
 
-                System.out.println("1번 명언이 등록되었습니다.");
+                quotationId++;
+                int id = quotationId;
+
+                System.out.printf("%d번 명언이 등록되었습니다.\n", id);
             }
         } // break; 로 끝나는 지점
     }
