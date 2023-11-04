dd27baea6fafb592bd5e446a598366033267b8ff
Son-Gyeongi <thsruddl77@gmail.com>
2023-11-04 16:27:39 +0900

    quotation practice2, 3 step

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -19,6 +19,8 @@ public class App {
                 s.nextLine();
                 System.out.print("작가 : ");
                 s.nextLine();
+
+                System.out.println("1번 명언이 등록되었습니다.");
             }
         } // break; 로 끝나는 지점
     }
