a05bd42974d35d1f263e0328cf0896d1af3716cd
Son-Gyeongi <thsruddl77@gmail.com>
2023-11-04 17:22:00 +0900

    quotation practice2, step 6-1 / id extraction 추출

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class App {
     Scanner s = new Scanner(System.in);
     int quotationId = 0;
-    List<Quotation> quotations = new ArrayList<>();
+    List<Quotation> quotations = new ArrayList<>(); // 명언 모음
 
     public void run() {
         System.out.println("== 명언 앱 ==");
@@ -23,6 +23,37 @@ public class App {
                 saveQuotation();
             } else if (cmd.equals("목록")) {
                 listQuotation();
+            } else if (cmd.startsWith("삭제?")) {
+                // startsWith() 괄호안에 시작하는 단어면 통과
+
+                int id =0;
+
+                List<String> paramName = new ArrayList<>();
+                List<String> paramValue = new ArrayList<>();
+
+                // 삭제?id=1&archive=true 에서 1만 추출하기, Bits는 조각이라는 뜻
+                String[] cmdBits = cmd.split("\\?", 2);
+                String action = cmdBits[0]; // 삭제
+                String queryString = cmdBits[1]; // id=1&archive=true
+
+                String[] queryStringBits = queryString.split("&");
+
+                // '&'에서는 여러 개 나오니깐 for문으로
+                for (int i = 0; i < queryStringBits.length; i++) {
+                    String[] param = queryStringBits[i].split("=", 2);
+                    paramName.add(param[0]);
+                    paramValue.add(param[1]);
+                }
+
+                // paramName에서 'id' 찾기
+                for (int i = 0; i < paramName.size(); i++) {
+                    if (paramName.get(i).equals("id")) {
+                        id = Integer.parseInt(paramValue.get(i));
+                        break;
+                    }
+                }
+
+                System.out.printf("%d번 명언이 삭제되었습니다.\n", id);
             }
         } // break; 로 끝나는 지점
     }
